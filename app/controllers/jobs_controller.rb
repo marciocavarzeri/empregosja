a6d1099d0aa43e45fc01a84c0e131cff667f96ac
Marcio Cavarzeri <marcio.cavarzeri@vagas.com.br>
2016-07-09 00:30:09 -0300

    Usuário cadastra vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,22 @@
 class JobsController < ApplicationController
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    job = Job.create(job_params)
+    redirect_to job
+  end
+
   def show
     @job = Job.find(params[:id])
   end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :location, :category, :company, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :jobs, only: [:new, :show]
+  resources :jobs, only: [:new, :show, :create]
 end
