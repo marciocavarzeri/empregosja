bd0028d4a2af3c9153d83747c04f08966c50777a
Marcio Cavarzeri <marcio.cavarzeri@vagas.com.br>
2016-07-14 11:17:55 -0300

    Visitante escolhe vagas por categoria - WIP

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,10 +19,24 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      flash.now[:notice] = 'Não foi possível atualizar a vaga'
+      render 'edit'
+    end
+  end
+
   private
 
   def job_params
-    params.require(:job).permit(:title, :location, :category, :company_id, :description, :featured)
+    params.require(:job).permit(:title, :location, :category_id, :company_id, :description, :featured)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :jobs, only: [:new, :show, :create]
+  resources :jobs, only: [:new, :show, :create, :edit, :update]
 end
