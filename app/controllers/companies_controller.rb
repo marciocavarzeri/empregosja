cff7e57f82f23dfef0426b4d8d984ada34cb9635
dev <dev@MacBook-PDP.local>
2016-07-16 18:00:37 -0300

    Usuário edita empresas

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,20 +1,44 @@
 class CompaniesController < ApplicationController
+  before_action :find_company, only: [:show, :edit, :update]
+
   def new
     @company = Company.new
   end
 
   def create
-    @company = Company.new(params.require(:company).permit(:name, :location, :phone, :mail))
+    @company = Company.new(company_params)
     @company.save
 
     redirect_to @company
   end
 
   def show
-    @company = Company.find(params[:id])
     @jobs = @company.jobs
     if @jobs.empty?
       flash.now[:notice] = 'Nenhuma vaga disponível'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @company.update(company_params)
+ 			redirect_to @company
+		else
+ 			flash.now[:notice] = 'Não foi possível atualizar a empresa'
+ 			render 'edit'
+ 		end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :location, :phone, :mail)
+  end
+
+  def find_company
+    @company = Company.find(params[:id])
+  end
+
 end
---
+++ spec/features/user_edit_companies_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'User edit company' do
+  scenario 'successfully' do
+    company = Company.create(name: 'Campus Code',
+                           location: 'São Paulo',
+                           mail: 'contato@campus.com.br',
+                           phone: '2369-3476')
+
+    visit edit_company_path(company)
+
+    fill_in 'Nome',        with: 'CampusCode RJ'
+    fill_in 'Local',       with: 'Rio de Janeiro'
+    fill_in 'Email',       with: 'contato@campuscode.com.br'
+    fill_in 'Telefone',    with: '2020-1234'
+
+    click_on 'Atualizar Empresa'
+
+    expect(page).to have_css('h1', text: 'CampusCode RJ')
+    expect(page).to have_content 'Rio de Janeiro'
+    expect(page).to have_content 'contato@campuscode.com.br'
+    expect(page).to have_content '2020-1234'
+  end
+
+  scenario 'with invalid data' do
+    company = Company.create(name: 'Campus Code',
+                           location: 'São Paulo',
+                           mail: 'contato@campus.com.br',
+                           phone: '2369-3476')
+
+    visit edit_company_path(company)
+
+    fill_in 'Nome', with: ''
+
+    click_on 'Atualizar Empresa'
+
+    expect(page).to have_content 'Não foi possível atualizar a empresa'
+  end
+
+end
